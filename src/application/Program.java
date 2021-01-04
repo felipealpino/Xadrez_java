97bd7d16daac768ff8992d0b11c290e7da17faf4
Felipe Alpino <felipealpino1@gmail.com>
2021-01-04 14:11:36 -0300

    First class: Position

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Position pos = new Position (5, 3);
+		System.out.println(pos);
+	}
+
+}
